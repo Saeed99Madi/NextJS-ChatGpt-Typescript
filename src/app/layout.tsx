2420eb6754664bbe14fc69c662d01dd8d04067fb
Saeed99Madi <saed.zooro@gmail.com>
2023-02-20 15:03:08 +0200

    build the project skeleton related to #1

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+import './globals.css';
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default RootLayout;
